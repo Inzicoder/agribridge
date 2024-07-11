16fcade93368731579107cca11f3994c8b2a0e24
Inzamam_ul_haque <inzamamul.h@myclnq.co.in>
2024-07-11 23:04:34 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>First Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
